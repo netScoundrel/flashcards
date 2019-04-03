cb2dc2fcbd69a3d292893e10ca845a63a8dcfb34
netScoundrel <estfrom@gmail.com>
2019-04-03 23:24:02 +0300

    redirects added

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const name = req.cookies.username;
+    if(name){
+        res.render('index', { name });
+    } else {
+        res.redirect('/hello');
+    }
+    
 });
 
 app.get('/cards', (req, res) => {
@@ -16,13 +24,24 @@ app.get('/cards', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-    res.render('hello');
+    const name = req.cookies.username;
+    if (name) {
+        res.redirect('/');
+    } else {
+        res.render('hello');
+    }
 });
 
 app.post('/hello', (req, res) => {
-    res.render('hello', { name: req.body.username })
+    res.cookie('username', req.body.username);
+    res.redirect('/')
 });
 
+app.post('/goodbye', (req, res) => {
+    res.clearCookie('username');
+    res.redirect('/hello');
+})
+
  app.listen(3000, () => {
-     console.log('The application is running on localhost:3000');
+    console.log('Server is running on localhots:3000')
  });
