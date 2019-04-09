de488fc1b7d1058e9c36c889ea32a977cc3e41d6
netScoundrel <estfrom@gmail.com>
2019-04-09 13:11:11 +0300

    Stylesheets added

--- app.js
+++ app.js
@@ -6,40 +6,26 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use('/static' ,express.static('public'));
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    const name = req.cookies.username;
-    if(name){
-        res.render('index', { name });
-    } else {
-        res.redirect('/hello');
-    }
-    
-});
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
 
-app.get('/cards', (req, res) => {
-    res.render('card', {prompt: "Who is burried in Grant's tomb"});
-});
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
 
-app.get('/hello', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.redirect('/');
-    } else {
-        res.render('hello');
-    }
-});
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+})
 
-app.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/')
-});
-
-app.post('/goodbye', (req, res) => {
-    res.clearCookie('username');
-    res.redirect('/hello');
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 })
 
  app.listen(3000, () => {
